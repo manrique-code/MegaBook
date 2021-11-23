a12569e1387a530c9763734f6489222a8dde9fcc
manrique-code <sergio.mnrq1@gmail.com>
2021-11-23 14:17:00 -0600

    CRUD de Libros 	- Se muestran todos los libros 	* Todavía no se ha testeado insertar, actualizar o eliminar 	  libros.

---
+++ src/Dao/Mnt/Libros.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Dao\Mnt;
+
+use Dao\Table;
+
+class Libros extends Table
+{
+    // Obtenemos todos los libros con sus categorias y autores.
+    public static function obtenerTodoLibros()
+    {
+        $sqlStr = "SELECT l.idlibros,
+                            l.nombreLibro,
+                            CONCAT(a.nombreAutor, a.apellidoAutor) as nombreAutor,
+                            GROUP_CONCAT(c.categoriaDes SEPARATOR ', ') as categorias
+                    FROM libros l
+                    INNER JOIN libros_autores la
+                    ON l.idlibros = la.idLibro
+                    INNER JOIN autores a
+                    on la.idAutor = a.idAutor
+                    INNER JOIN libros_categorias lc
+                    ON l.idlibros = lc.idLibro
+                    INNER JOIN categorias c
+                    on lc.idCategoria = c.idCategorias
+                    ORDER BY l.idlibros;";
+        return self::obtenerRegistros($sqlStr, array());
+    }
+    // Obtener todos los libros que están en el inventario.
+    public static function obtenerLibros()
+    {
+        $sqlStr = "SELECT * FROM libros;";
+        return self::obtenerRegistros($sqlStr, array());
+    }
+
+    // Obtenemos un libro por id.
+    public static function obtenerLibro($idlibros)
+    {
+        $sqlStr = "SELECT * FROM libros WHERE idlibros = :idlibros;";
+        return self::obtenerUnRegistro($sqlStr, array(
+            "idlibros" => $idlibros
+        ));
+    }
+
+    // Creamos un libro en la base de datos.
+    public static function crearLibro(
+        $nombreLibro,
+        $descripcion
+    ) {
+        $sqlStr = "INSERT INTO libros(nombreLibro, descripcion) VALUES(:nombreLibro, :descripcion);";
+        return self::executeNonQuery($sqlStr, array(
+            "nombreLibro" => $nombreLibro,
+            "descripcion" => $descripcion
+        ));
+    }
+
+    // Editamos un libro de la base de datos.
+    public static function editarLibro(
+        $nombreLibro,
+        $descripcion,
+        $idlibros
+    ) {
+        $sqlStr = "UPDATE libros SET nombreLibro = :nombreLibro, descripcion = :descripcion WHERE idlibros = :idlibros;";
+        return self::executeNonQuery($sqlStr, array(
+            "nombreLibro" => $nombreLibro,
+            "descripcion" => $descripcion,
+            "idlibros" => $idlibros
+        ));
+    }
+
+    // Eliminamos un libro.
+    public static function eliminarLibro(
+        $idlibros
+    ) {
+        $sqlStr = "DELETE FROM libros WHERE idlibros = :idlibros;";
+        return self::executeNonQuery($sqlStr, array(
+            "idlibros" => $idlibros
+        ));
+    }
+}
