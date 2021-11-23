31a24694064a94bc0ce394a9423d030b75d9393f
manrique-code <sergio.mnrq1@gmail.com>
2021-11-23 13:53:05 -0600

    Mostramos todos los libros de la base de datos en el WW 	- Se muestran la tabla de items 	- Redirige a la página para creación, edición, visualización y 	  eliminación individual.

--- src/Controllers/Mnt/Libros.php
+++ src/Controllers/Mnt/Libros.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Controllers\Mnt;
+
+use Controllers\PrivateController;
+use Views\Renderer;
+
+class Libros extends PrivateController
+{
+    public function run(): void
+    {
+        $viewData = array();
+        // dd(\Dao\Mnt\Libros::obtenerTodoLibros());
+        $viewData["items"] = \Dao\Mnt\Libros::obtenerTodoLibros();
+        // Verificamos si el usuario está autorizado para utilizar las funciones.
+        $viewData["new_enabled"] = self::isFeatureAutorized("Controllers\Mnt\Libros\New");
+        $viewData["edit_enabled"] = self::isFeatureAutorized("Controllers\Mnt\Libros\Edit");
+        $viewData["delete_enabled"] = self::isFeatureAutorized("Controllers\Mnt\Libros\Delete");
+
+        Renderer::render("mnt/libros", $viewData);
+    }
+}
