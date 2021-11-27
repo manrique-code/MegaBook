2f4cc25e3f8c3c074fbb00003feffaa209adbd27
manrique-code <sergio.mnrq1@gmail.com>
2021-11-26 21:20:58 -0600

    Implementado controlador del libros y autores
    Se puede:
    - Insertar un autor desde el dropdown menú
    - Eliminar un autor desde los items de autores

---
+++ src/Controllers/Mnt/LibroAutores.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Controllers\Mnt;
+
+use Controllers\PrivateController;
+use Views\Renderer;
+
+class LibroAutores extends PrivateController
+{
+    private function nope()
+    {
+        \Utilities\Site::redirectToWithMsg(
+            "index.php?page=mnt_roles",
+            "Ocurrió algo inesperado. Intente nuevamente."
+        );
+    }
+
+    private function yeah($idlibros)
+    {
+        \Utilities\Site::redirectToWithMsg(
+            "index.php?page=mnt_libroautores&mode=DSP&idlibros=$idlibros",
+            "¡Operación ejecutada satisfactoriamente!"
+        );
+    }
+
+    public function run(): void
+    {
+        $viewData = array(
+            "mode" => "",
+            "idlibros" => "",
+            "nombreLibro" => "",
+            "idAutor" => "",
+            "nombreAutor" => "",
+            "autor" => array(),
+            "noAutor" => array()
+        );
+
+        if (isset($_GET["mode"])) {
+            $viewData["mode"] = $_GET["mode"];
+        } else $this->nope();
+        if (isset($_GET["idlibros"])) $viewData["idlibros"] = $_GET["idlibros"];
+        else if ($viewData["mode"] !== "INS") $this->nope();
+
+        if (isset($viewData["mode"])) {
+            $viewData["autor"] = \Dao\Mnt\LibroAutores::obtenerAutoresPorLibro($viewData["idlibros"]);
+            $viewData["noAutor"] = \Dao\Mnt\LibroAutores::obtenerNoAutorPorLibro($viewData["idlibros"]);
+            $viewData["idAutor"] = (isset($_GET["idautor"])) ? $_GET["idautor"] : "";
+            switch ($viewData["mode"]) {
+                case "INS":
+                    // dd($viewData);
+                    if (\Dao\Mnt\LibroAutores::crearLibroConAutor(
+                        $viewData["idlibros"],
+                        $viewData["idAutor"]
+                    )) $this->yeah($viewData["idlibros"]);
+                    break;
+                case "DEL":
+                    if (\Dao\Mnt\LibroAutores::eliminarLibroConAutor($viewData["idAutor"])) $this->yeah($viewData["idlibros"]);
+                    break;
+            }
+        }
+        Renderer::render("mnt/libroautor", $viewData);
+    }
+}
